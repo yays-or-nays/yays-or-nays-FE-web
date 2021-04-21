b2680a7c26c6c4ba47c5e7f33e4fb8b00fcd4764
mattdean <matthewallandean@gmail.com>
2021-04-21 11:37:33 -0600

    Solve cors issue

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -4,30 +4,31 @@ import './Categories.css';
 import axios from 'axios';
 import {query} from 'gql-query-builder';
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var graphql = JSON.stringify({
+  query: "query{\n  tags{\n      id\n      category\n  }\n}\n",
+  variables: {}
+})
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: graphql,
+  redirect: 'follow'
+};
+
 export default function Categories() {
   const [categories, setCategories] = useState([])
 
   useEffect(() => {
-    axios
-      .post(
-        'https://yays-or-nays-be.herokuapp.com/graphql',
-        query({
-          operation: 'tags',
-          fields: ['id', 'category'],
-        }),
-      )
-      .then(response => {
-        try {
-          console.log(response);
-          setCategories(response.data.data.tags);
-        } 
-        catch {
-          return response;
-        }
-      });
+    fetch("https://yays-or-nays-be.herokuapp.com/graphql", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }, [])
 
-  const categoryButtons = categories.map(category => {
+  const categoryButtons = categories ? categories.map(category => {
     return (
       <section
         className='category-button'
@@ -39,7 +40,7 @@ export default function Categories() {
         </p>
       </section>
     );
-  });
+  }) : <p>Loading...</p>;
 
   return (
     <main>
@@ -47,3 +48,7 @@ export default function Categories() {
     </main>
   )
 }
+
+
+
+
