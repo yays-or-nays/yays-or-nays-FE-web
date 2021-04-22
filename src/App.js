ff725572caeedf2321fd1d3a49c2393a96b5ff54
mattdean <matthewallandean@gmail.com>
2021-04-22 09:50:38 -0600

    Add unknown things

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import Categories from './Categories/Categories';
 import HotTake from './HotTake/HotTake';
 
 function App() {
-  const [hotTake, setHotTake] = useState({});
+  const [hotTake, setHotTake] = useState(null);
 
   const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-  const graphql = JSON.stringify({
-    query: "query{\n  hotTakeByCategory(tagId: 10) {\n    id\n    question\n    yesVote\n    noVote\n    picture\n    tag{\n      id\n      category\n    }\n  }\n}\n\n",
-    variables: {}
-  })
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: graphql,
-    redirect: 'follow'
-  };
+  myHeaders.append("Content-Type", "application/json");
 
   const getHotTake = id => {
-    fetch("https://yays-or-nays-be.herokuapp.com/graphql", requestOptions)
+    fetch("https://yays-or-nays-be.herokuapp.com/graphql", {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify({
+        query: `query{\n  hotTakeByCategory(tagId: ${id}) {\n    id\n    question\n    yesVote\n    noVote\n    picture\n    tag{\n      id\n      category\n    }\n  }\n}\n\n`,
+        variables: {}
+      }),
+      redirect: 'follow'
+    })
       .then(response => response.json())
       .then(result => setHotTake(result.data.hotTakeByCategory))
       .catch(error => console.log('error', error));
